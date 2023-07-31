7313cb4655acab12ce771f41ba19b2a3c3adb775
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2023-07-31 16:20:53 +1000

    Add ids to index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+const captcha = document.getElementById("captcha");
+const image = document.getElementById("image");
+const input = document.getElementById("input");
+const DOCreturn = document.getElementById("return");
+
+
+
+function captchaCheck() {
+    // Put something here
+}
