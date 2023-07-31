b2248de9e506465b08ecbf5462161ffec289c404
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2023-07-31 16:59:00 +1000

    add first image - working on script

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const captcha = document.getElementById("captcha");
 const image = document.getElementById("image");
-const input = document.getElementById("input");
+const inputArea = document.getElementById("inputArea");
 const DOCreturn = document.getElementById("return");
 
-
+const imageDict = {
+    "photo-1578885136359-16c8bd4d3a8e.png":
+        ``
+}
 
 function captchaCheck() {
     // Put something here
